7fed7aecb7db882ca422e9af2726a12f0a975fa4
jenoandroid19 <jeno.micheal@anywhere.co>
2022-11-15 19:57:05 +0530

    added files

---
+++ app/src/main/java/com/plcoding/stockmarketapp/domain/model/CompanyListing.kt
@@ -0,0 +1,8 @@
+package com.plcoding.stockmarketapp.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+data class CompanyListing(val name: String,
+                          val symbol: String,
+                          val exchange: String)
